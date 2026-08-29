5798104d7278aab1c3ab658081c949ade94cbcfb
Capitão Assumção <assumcao@gmail.com>
2026-08-29 07:29:36 -0300

    pautas: inclui rodada 24 no marcador automático

--- scripts/marcar-pautas-publicadas.mjs
+++ scripts/marcar-pautas-publicadas.mjs
@@ -12,7 +12,9 @@ const publicadas = new Map([
   ['8e8ca6451691d7b6', 'dupla-presa-furto-cabos-alta-tensao-bento-ferreira-vitoria'],
   ['f7e86b04c221f172', 'professor-fabian-senado-es-educacao-seguranca-psol-2026'],
   ['031c4f772d1247af', 'tse-forcas-federais-cinco-estados-eleicoes-2026'],
-  ['11effe998561c3b8', 'mapa-estadual-pl-psd-mdb-governos-eleicoes-2026']
+  ['11effe998561c3b8', 'mapa-estadual-pl-psd-mdb-governos-eleicoes-2026'],
+  ['ceb3868fbeb99d30', 'condenado-desacato-desobediencia-preso-dez-anos-depois-castelo'],
+  ['f16c9ccbc83be345', 'mulher-ferida-garrafas-briga-cruzeiro-do-sul-cariacica']
 ]);
 
 for (const pauta of dados.pautas || []) {
