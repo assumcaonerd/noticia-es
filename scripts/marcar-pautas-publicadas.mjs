0a177bf302315a9e1c88d496f529caafb004d4db
Capitão Assumção <assumcao@gmail.com>
2026-08-29 02:30:35 -0300

    pautas: inclui rodada editorial 20

--- scripts/marcar-pautas-publicadas.mjs
+++ scripts/marcar-pautas-publicadas.mjs
@@ -5,7 +5,9 @@ const dados = JSON.parse(await fs.readFile(arquivo, 'utf8'));
 const agora = new Date().toISOString();
 const publicadas = new Map([
   ['51970d7c56b30607', 'flavio-lidera-lula-no-es-quaest-34-a-29-empate-tecnico'],
-  ['d553cd035abffead', 'ricardo-ferraco-lidera-e-venceria-segundo-turno-no-es-quaest']
+  ['d553cd035abffead', 'ricardo-ferraco-lidera-e-venceria-segundo-turno-no-es-quaest'],
+  ['2980f76f6b342905', 'golpe-audios-voz-amigas-serra-invade-celulares-pix'],
+  ['f53bb40a057dbc99', 'tre-rj-rejeita-denuncia-crivella-qg-propina-senado-2026']
 ]);
 
 for (const pauta of dados.pautas || []) {
