3b89d9f8161fac3e219d79a144874daa2e451e55
Capitão Assumção <assumcao@gmail.com>
2026-09-01 07:55:58 -0300

    Remove duplicatas editoriais do feed

--- remover-materias-20260831.js
+++ remover-materias-20260831.js
@@ -1,9 +1,16 @@
-/* NOTÍCIA ES - remoções editoriais 31/08/2026 */
+/* NOTÍCIA ES - remoções editoriais e deduplicação */
 (function () {
   if (typeof noticias === "undefined" || !Array.isArray(noticias)) return;
   const removidos = new Set([
     "kremlin-russia-avanco-frente-ucrania-peskov-alegacao",
-    "russia-seguranca-europa-peskov-arquitetura-negociacoes"
+    "russia-seguranca-europa-peskov-arquitetura-negociacoes",
+
+    /* Duplicatas: manter 29-candidatos-usam-bolsonaro-nome-urna-eleicoes-2026 */
+    "29-candidatos-bolsonaro-nome-urna-eleicoes-2026",
+    "29-candidatos-usam-bolsonaro-nome-de-urna-eleicoes-2026",
+
+    /* Duplicata: manter antonio-gobbi-morte-vitoria-contas-bancarias-investigacao */
+    "antonio-gobbi-morte-canal-vitoria-investigacao-tentativas-bancarias"
   ]);
   for (let i = noticias.length - 1; i >= 0; i--) {
     if (removidos.has(noticias[i] && noticias[i].slug)) noticias.splice(i, 1);
