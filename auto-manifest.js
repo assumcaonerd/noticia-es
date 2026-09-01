36eb5f786c6024e9b3f7df05c46872e057bd5a7e
Capitão Assumção <assumcao@gmail.com>
2026-09-01 07:04:54 -0300

    Adiciona shard da rodada de 1 de setembro

--- auto-manifest.js
+++ auto-manifest.js
@@ -1,4 +1,5 @@
 const noticiasAutoArquivos = [
+  "auto-redacao-20260901-070400.js",
   "auto-redacao-20260901-065100.js",
   "auto-redacao-20260901-090423.js",
   "auto-redacao-20260901-110500.js",
