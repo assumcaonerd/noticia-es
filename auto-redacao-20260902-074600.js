4b44d6681a45295469f1ded5d92bb25a74e499dd
Capitão Assumção <assumcao@gmail.com>
2026-09-02 07:46:41 -0300

    publicacao: publica rodada aprovada das 07h45

---
+++ auto-redacao-20260902-074600.js
@@ -0,0 +1,3 @@
+// Rodada aprovada pelo editor em 02/09/2026.
+document.write('<script src="redacao-pronta/rodada-20260902-074500.js"><' + '/script>');
+document.write('<script src="publicar-redacao-20260902-074600.js"><' + '/script>');
