30301290db07777654adfe950b9597d91735420e
Capitão Assumção <assumcao@gmail.com>
2026-09-01 21:08:26 -0300

    redacao: adiciona shard automatico de 21h02 ao manifesto

--- auto-manifest.js
+++ auto-manifest.js
@@ -1,4 +1,5 @@
 const noticiasAutoArquivos = [
+  "auto-redacao-20260901-210214.js",
   "auto-redacao-20260901-200457.js",
   "auto-redacao-20260901-190730.js",
   "auto-redacao-20260901-180242.js",
