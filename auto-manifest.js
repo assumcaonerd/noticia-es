2a7b1c516d127a576e59f44b23cc3d1826dc6536
Capitão Assumção <assumcao@gmail.com>
2026-09-03 14:56:58 -0300

    publica(manual): inclui matéria Lula, Biomm e Vorcaro no feed

--- auto-manifest.js
+++ auto-manifest.js
@@ -1,4 +1,5 @@
 const noticiasAutoArquivos = [
+  "auto-redacao-20260903-143600.js",
   "auto-redacao-20260903-143500.js",
   "auto-redacao-20260902-221600.js",
   "auto-redacao-20260902-092800.js",
