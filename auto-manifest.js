e9c2a408691efe83c6b3587d41ae2d4c64aca969
Capitão Assumção <assumcao@gmail.com>
2026-09-05 15:48:01 -0300

    feat: inclui nova matéria no manifesto

--- auto-manifest.js
+++ auto-manifest.js
@@ -1,4 +1,5 @@
 const noticiasAutoArquivos = [
+  "auto-redacao-20260905-154500.js",
   "auto-redacao-20260904-085400.js",
   "auto-redacao-20260903-235800.js",
   "auto-redacao-20260903-162100.js",
