e8ade0e6855df4d34a1987c5568df5365edc8d68
Capitão Assumção <assumcao@gmail.com>
2026-09-13 14:31:31 -0300

    capa: usa o PNG enviado em assets/og/elianecantanhede.png

--- cantanhede-capa-fix.js
+++ cantanhede-capa-fix.js
@@ -1,15 +1,31 @@
 (function () {
-  var capaAnexo = "https://noticiaes.com.br/assets/og/elianecantanhede.png";
-  var capaSvg = "https://noticiaes.com.br/assets/og/elianecantanhede.svg";
-  var capaColuna = "https://pbs.twimg.com/media/HSGOnUpWwAMIi3w.jpg";
+  var capa = "https://noticiaes.com.br/assets/og/elianecantanhede.png";
   var slug = "dos-aplausos-ao-cadafalso-a-militante-de-redacao-troca-o-xerife-quando-o-xerife-vira-estorvo";
-  var capa = capaColuna;
   if (typeof noticias !== "undefined" && Array.isArray(noticias)) {
     noticias.forEach(function (n) {
       if (n.slug === slug) {
         n.imagem = capa;
         n.imagemX = capa;
+        n.imagemLargura = 1672;
+        n.imagemAltura = 941;
+        n.legendaImagem = "Dos aplausos ao cadafalso: a militante de redação troca o xerife quando o xerife vira estorvo";
       }
     });
   }
+  function apply() {
+    document.querySelectorAll("img").forEach(function (img) {
+      var s = img.getAttribute("src") || "";
+      if (
+        s.indexOf("HSGOnUp") !== -1 ||
+        s.indexOf("elianecantan") !== -1 ||
+        (img.className || "").indexOf("materia-capa") !== -1
+      ) {
+        if (img.src !== capa) img.src = capa;
+      }
+    });
+  }
+  apply();
+  document.addEventListener("DOMContentLoaded", apply);
+  setTimeout(apply, 150);
+  setTimeout(apply, 700);
 })();
