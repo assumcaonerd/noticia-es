71757ec3bf400d319abd3ac55085604ceed14279
Capitão Assumção <assumcao@gmail.com>
2026-09-20 12:44:33 -0300

    publica: propostas de Grassi para imposto único e facções

--- auto-manifest.js
+++ auto-manifest.js
@@ -1,4 +1,5 @@
 const noticiasAutoArquivos = [
+  "auto-redacao-20260920-123744.js",
   "auto-redacao-20260920-114113.js",
   "auto-redacao-20260920-104026.js",
   "auto-redacao-20260920-094126.js",
